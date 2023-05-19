c08ac4750919eb850ce86d0c6746c66907cf1f06
InsulatorGMan <76236444+InsulatorGMan@users.noreply.github.com>
2023-05-19 13:18:26 +0000

    client

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+export default function App() {
+
+  return (
+
+    <div className="App">
+
+      <div className="TopBar">
+
+        <image className="Bar_Logo"></image>
+
+      </div>
+      <div className="Messages"></div>
+      <div className="BottonBar">
+
+        <input className="Chat_Input" placeholder="Type Here."></input>
+
+      </div>
+
+
+    </div>
+
+  )
+
+}
